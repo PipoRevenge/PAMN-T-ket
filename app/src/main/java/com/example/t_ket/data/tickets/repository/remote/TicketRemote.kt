ae70b3fbc2d29d26a97cc0ca583a9d0a6cc712fe
PipoRevenge <95238885+PipoRevenge@users.noreply.github.com>
2023-11-22 18:02:40 +0000

    Revert "Dev firebase"

--- app/src/main/java/com/example/t_ket/data/tickets/repository/remote/TicketRemote.kt
+++
@@ -1,7 +0,0 @@
-package com.example.t_ket.data.tickets.repository.remote
-import com.example.t_ket.data.tickets.model.Ticket
-
-interface TicketRemote {
-    fun getTicketsFromFirebase(id_event: String)
-    fun updateTicketStatusInFirebase(id_event:String, id: String, status: Boolean)
-}
--- app/src/main/java/com/example/t_ket/data/users/model/User.kt
+++
@@ -1,5 +0,0 @@
-package com.example.t_ket.data.users.model
-//Aqui  va los datos del usuario
-data class  User (
-    var codeofEvent: String,
-)
